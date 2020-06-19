e278eb4f188bc85ee7b0dfb1355e15896d4de3c1
SuboOx <zjuzihaocao@gmail.com>
2020-06-18 21:13:18 -0400

    Implemented multi-thread server.

--- src/Server.java
+++ src/Server.java
@@ -2,6 +2,54 @@ import java.net.*;
 import java.io.*;
 
 public class Server {
+    private static DataBase db = new DataBase();
+    private static Protocol protocol = new Protocol();
+
+    static class ServerThread extends Thread {
+        protected Socket clientSocket;
+
+        public ServerThread(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            PrintWriter out = null;
+            BufferedReader in = null;
+            try {
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            } catch (IOException e) {
+                System.err.println("Unable to create buffer");
+                return;
+            }
+
+            String inputLine;
+
+            while (true) {
+                try {
+                    inputLine = in.readLine();
+                    if (inputLine != null) {
+                        parseResult parsed = protocol.unpack(inputLine);
+                        System.out.println("Received msg from client " + parsed.clientID + ":" + inputLine);
+                        System.out.println("Unpacked msg: " + parsed.var + "=" + parsed.value + ", id: " + parsed.id);
+                        db.setVariable(parsed.var, parsed.value);
+                        //print database
+                        System.out.println("Current database:");
+                        db.listAll();
+                    } else {
+                        clientSocket.close();
+                        return;
+                    }
+                } catch (IOException e) {
+                    System.err.println("Unable to readline.");
+                    return;
+                }
+            }
+
+        }
+    }
+
     public static void main(String[] args) throws IOException {
 
         if (args.length != 1) {
@@ -13,33 +61,28 @@ public class Server {
 
         /*Database and protocol here*/
         System.out.println("Initializing database and protocol...");
-        DataBase db = new DataBase();
-        Protocol protocol = new Protocol();
         System.out.println("Done.");
 
-        try (ServerSocket serverSocket = new ServerSocket(portNumber);
-             Socket clientSocket = serverSocket.accept();
-             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
 
-            String inputLine;
-
-            while ((inputLine = in.readLine()) != null) {
-                parseResult parsed = protocol.unpack(inputLine);
-                System.out.println("Received msg from client " + parsed.clientID + ":" + inputLine);
-                System.out.println("Unpacked msg: " + parsed.var + "=" + parsed.value + ", id: " + parsed.id);
-                db.setVariable(parsed.var, parsed.value);
-                //print database
-                System.out.println("Current database:");
-                db.listAll();
-            }
-            clientSocket.close();
-            System.out.println("Ready for next msg");
+        try {
+            serverSocket = new ServerSocket(portNumber);
         } catch (IOException e) {
             System.out.println(
                     "Exception caught when trying to listen on port " + portNumber + " or listening for a connection");
             System.out.println(e.getMessage());
         }
 
+        while (true) {
+            try {
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.out.println("Error: " + e);
+            }
+            // new thread for a client
+            new ServerThread(clientSocket).start();
+        }
     }
 }
+
