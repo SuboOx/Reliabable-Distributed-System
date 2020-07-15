215c5126561bb7aa867b5a711502f54b53f5527e
xuemeibai <59850121+xuemeibai@users.noreply.github.com>
2020-07-14 21:27:10 -0400

    Packed some part of the functions in LFD to helper functions.

--- src/LFD.java
+++ src/LFD.java
@@ -12,45 +12,70 @@ public class LFD {
     private static int heartbeatCount = 0;
     private static int LFDID;
     private static String hostName;
+    private static int port;
+    private static int serverID;
+    private static int timeout;
+    private static int heartbeatFreq;
+    private static boolean lastStatus;
+    private static  boolean nowStatus;;
+
+    static class LFDHelper {
+        static void readConsoleInput(String[] args) {
+
+            if (args.length != 5 && args.length != 4) {
+                System.err.println("Usage: java LFD <LFD id> <host name> <serverID> <time out> (<heartbeat freq>)");
+                System.exit(1);
+            }
+            hostName = args[1];
+            LFDID = Integer.parseInt(args[0]);
+            serverID = Integer.parseInt(args[2]);
+            port = serverConstant.portNumber[serverID];
+            timeout = Integer.parseInt(args[3]);
+
+           heartbeatFreq = 2000;
+           lastStatus = false;
+
+
+            //choose default or set a heartbeat frequency
+            if (args.length == 5) {
+                heartbeatFreq = Integer.parseInt(args[4]);// Start from 0
+            }
+
+            //choose default or set a heartbeat frequency
+            if (args.length == 5) {
+                heartbeatFreq = Integer.parseInt(args[4]);// Start from 0
+            }
+
+
+        }
+
+        static void printWaitMsg() {
+
+            //Wait the server to on line
+            while (!isAlive(hostName, port, timeout)) {
+                System.out.println("Waiting for Server to be launched");
+                lastStatus = false;
+            }
+        }
+    }
 
     public static void main(String[] args) throws InterruptedException {
 
-        if (args.length != 5 && args.length != 4) {
-            System.err.println("Usage: java LFD <LFD id> <host name> <serverID> <time out> (<heartbeat freq>)");
-            System.exit(1);
-        }
-        hostName = args[1];
-        LFDID = Integer.parseInt(args[0]);
-        int serverID = Integer.parseInt(args[2]);
-        int port = serverConstant.portNumber[serverID];
-        int timeout = Integer.parseInt(args[3]);
-        int heartbeatFreq = 2000;
-        boolean laststatus = false;
-        boolean nowstatus;
-
-        //choose default or set a heartbeat frequency
-        if (args.length == 5) {
-            heartbeatFreq = Integer.parseInt(args[4]);// Start from 0
-        }
-
-        //Wait the server to on line
-        while (!isAlive(hostName, port, timeout)) {
-            System.out.println("Waiting for Server to be launched");
-            laststatus = false;
-        }
+       LFDHelper.readConsoleInput(args);
+       LFDHelper.printWaitMsg();
 
         //Check if server is alive
         while (true) {
-            nowstatus = log(isAlive(hostName, port, timeout));
+            nowStatus = log(isAlive(hostName, port, timeout));
 
-            if (nowstatus ^ laststatus) {
+            if (nowStatus ^ lastStatus) {
                 //send message to GFD
                 try (Socket s = new Socket(hostName, serverConstant.GFDport);) {
                     System.err.println("Successfully connected to " + hostName + " : " + serverConstant.GFDport);
                     InputStream is = s.getInputStream();
                     OutputStream os = s.getOutputStream();
                     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os));
-                    if (!laststatus) {
+                    if (!lastStatus) {
                         //Add replica
                         bw.write(Protocol.LFDPack(LFDID, "add", serverID));
                         bw.flush();
@@ -66,8 +91,8 @@ public class LFD {
                 }
 
             }
-            //send2GFD(nowstatus);
-            laststatus = nowstatus;
+            //send2GFD(nowStatus);
+            lastStatus = nowStatus;
 
             Thread.sleep(heartbeatFreq);
         }
