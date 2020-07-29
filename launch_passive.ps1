64047ebb5526d46471790423cde8434ba0cc45d7
SuboOx <zjuzihaocao@gmail.com>
2020-07-29 19:28:05 -0400

    Add powershell scripts to launch the system.

---
+++ launch_passive.ps1
@@ -0,0 +1,49 @@
+# Powershell script for initializing the whole system
+Write-Host "Starting System"
+Set-Location ./out/production/ReliableDS
+# Launch GFD
+Write-Host "Launching GFD..."
+Start-Process java -ArgumentList 'GFD', '8891'
+Write-Host "GFD Launched"
+
+# Launch LFDs
+Write-Host "Launching LFD 0..."
+Start-Process java -ArgumentList 'LFD', '0', '127.0.0.1', '0', '2000', '1000'
+Write-Host "LFD 0 Launched"
+
+Write-Host "Launching LFD 1..."
+Start-Process java -ArgumentList 'LFD', '1', '127.0.0.1', '1', '2000', '1000'
+Write-Host "LFD 1 Launched"
+
+Write-Host "Launching LFD 2..."
+Start-Process java -ArgumentList 'LFD', '2', '127.0.0.1', '2', '2000', '1000'
+Write-Host "LFD 2 Launched"
+
+# Launch servers
+Write-Host "Launching Server 0..."
+Start-Process java -ArgumentList 'Server', '0', '8888', 'primary', '10000'
+Write-Host "Server 0 Launched"
+
+Write-Host "Launching Server 1..."
+Start-Process java -ArgumentList 'Server', '1', '8889', 'backup', '10000'
+Write-Host "Server 1 Launched"
+
+Write-Host "Launching Server 2..."
+Start-Process java -ArgumentList 'Server', '2', '8890', 'backup', '10000'
+Write-Host "Server 2 Launched"
+
+# Launch clients
+Write-Host "Launching Client 0..."
+Start-Process java -ArgumentList 'Client', '0', '127.0.0.1', '0', '1', '2'
+Write-Host "Client 0 Launched"
+
+Write-Host "Launching Client 0..."
+Start-Process java -ArgumentList 'Client', '1', '127.0.0.1', '0', '1', '2'
+Write-Host "Client 0 Launched"
+
+Write-Host "Launching Client 0..."
+Start-Process java -ArgumentList 'Client', '2', '127.0.0.1', '0', '1', '2'
+Write-Host "Client 0 Launched"
+
+Set-Location ../../..
+#>
