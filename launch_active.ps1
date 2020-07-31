df717099116073425295c7c5bd585af532680121
SuboOx <zjuzihaocao@gmail.com>
2020-07-31 17:04:34 -0400

    Update script.

--- launch_active.ps1
+++ launch_active.ps1
@@ -7,53 +7,68 @@ Write-Host "Launching RM..."
 Start-Process java -ArgumentList 'ReplicaManager', 'active'
 Write-Host "RM Launched"
 
+Start-Sleep -Seconds 3
 # Launch GFD
 Write-Host "Launching GFD..."
 Start-Process java -ArgumentList 'GFD', '8891'
 Write-Host "GFD Launched"
+
+Start-Sleep -Seconds 3
 
 # Launch LFDs
 Write-Host "Launching LFD 0..."
 Start-Process java -ArgumentList 'LFD', '0', '127.0.0.1', '0', '2000', '1000'
 Write-Host "LFD 0 Launched"
 
+Start-Sleep -Seconds 3
+
 Write-Host "Launching LFD 1..."
 Start-Process java -ArgumentList 'LFD', '1', '127.0.0.1', '1', '2000', '1000'
 Write-Host "LFD 1 Launched"
 
+Start-Sleep -Seconds 3
+
 Write-Host "Launching LFD 2..."
 Start-Process java -ArgumentList 'LFD', '2', '127.0.0.1', '2', '2000', '1000'
 Write-Host "LFD 2 Launched"
+
+Start-Sleep -Seconds 3
 
 # Launch servers
 Write-Host "Launching Server 0..."
 Start-Process java -ArgumentList 'Server', '0', '8888'
 Write-Host "Server 0 Launched"
 
-Start-Sleep -Seconds 1
+Start-Sleep -Seconds 3
 
 Write-Host "Launching Server 1..."
 Start-Process java -ArgumentList 'Server', '1', '8889'
 Write-Host "Server 1 Launched"
 
-Start-Sleep -Seconds 1
+Start-Sleep -Seconds 3
 
 Write-Host "Launching Server 2..."
 Start-Process java -ArgumentList 'Server', '2', '8890'
 Write-Host "Server 2 Launched"
+
+Start-Sleep -Seconds 3
 
 # Launch clients
 Write-Host "Launching Client 0..."
 Start-Process java -ArgumentList 'Client', '0', '127.0.0.1', '0', '1', '2'
 Write-Host "Client 0 Launched"
 
-Write-Host "Launching Client 0..."
-Start-Process java -ArgumentList 'Client', '1', '127.0.0.1', '0', '1', '2'
-Write-Host "Client 0 Launched"
+Start-Sleep -Seconds 3
 
-Write-Host "Launching Client 0..."
+Write-Host "Launching Client 1..."
+Start-Process java -ArgumentList 'Client', '1', '127.0.0.1', '0', '1', '2'
+Write-Host "Client 1 Launched"
+
+Start-Sleep -Seconds 3
+
+Write-Host "Launching Client 2..."
 Start-Process java -ArgumentList 'Client', '2', '127.0.0.1', '0', '1', '2'
-Write-Host "Client 0 Launched"
+Write-Host "Client 2 Launched"
 
 Set-Location ../../..
 #>
